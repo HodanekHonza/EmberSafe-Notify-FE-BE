6a34de19d42337d5253464d2c55936a4d14c80bf
Honza Hodánek <hodanek.honza@gmail.com>
2024-04-02 22:58:30 +0200

    added default abl from project before, tomorrow gonna refactor

--- server/abl/room/create-abl.js
+++ server/abl/room/create-abl.js
@@ -20,6 +20,27 @@ let schema = {
 };
 
 async function CreateAbl(req, res) {
+   try {
+        const ajv = new Ajv();
+        const valid = ajv.validate(schema, req.body);
+        if (valid) {
+            let video = req.body;
+            video = await dao.createVideo(video);
+            res.json(video);
+        } else {
+            res.status(400).send({
+                errorMessage: "validation of input failed",
+                params: req.body,
+                reason: ajv.errors,
+            });
+        }
+    } catch (e) {
+        if (e.includes("Video with name ")) {
+            res.status(400).send({ errorMessage: e, params: req.body });
+        } else {
+            res.status(500).send(e);
+        }
+    }
 }
 
 module.exports = CreateAbl;
