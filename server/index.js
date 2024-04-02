81dbcb820e1d7c58961cc10c4da67089cc4c497c
Honza Hodánek <hodanek.honza@gmail.com>
2024-04-02 20:08:02 +0200

    added timestamp to temperaturre method

--- server/index.js
+++ server/index.js
@@ -8,32 +8,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-let temperatureData = null; // Variable to store temperature data
-
+let temperatureData = null;
+let temperatureTimeStamp = null;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
-app.post('/postTemperature', (req, res) => {
-    // Assuming you receive temperature data in the request body
-    temperatureData = req.body.temp;
-    console.log("Received temperature data:", temperatureData);
-    res.send("Temperature data received successfully!");
+app.post("/postTemperature", (req, res) => {
+  temperatureData = req.body.temp;
+  temperatureTimeStamp = new Date();
+  console.log("Received temperature data:", temperatureData);
+  res.send("Temperature data received successfully!");
 });
 
-app.get('/seeTemperature', (req, res) => {
-    if (temperatureData !== null) {
-        res.json({ temp: temperatureData });
-    } else {
-        res.status(404).send("No temperature data available.");
-    }
+app.get("/seeTemperature", (req, res) => {
+  if (temperatureData !== null) {
+    res.json({ temp: temperatureData, timestamp: temperatureTimeStamp });
+  } else {
+    res.status(404).send("No temperature data available.");
+  }
 });
 
-// Serve the temperature.html file
-app.get('/temperature.html', (req, res) => {
-  res.sendFile(__dirname + '/temperature.html');
+app.get("/temperature.html", (req, res) => {
+  res.sendFile(__dirname + "/temperature.html");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
