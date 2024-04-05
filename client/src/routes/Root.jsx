bf7cab62a0937b10c7c13051fe34065c2ab0aa88
Honza Hodánek <hodanek.honza@gmail.com>
2024-04-05 20:24:49 +0200

    created concrete routes, for landing page and dashboard. also created more routes to acompany the newely created landing page

--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
 
-export default function RoomsPage() {
+export default function Root() {
   const paramsForRooms = useParams();
   console.log(paramsForRooms)
   return (
     <div>
-      RoomsPage
+      <h1>NAVBAR</h1>
+      <div id="detail">
+            <Outlet />
+        </div>
+      <h1>FOOTER</h1>
     </div>
   )
 }
